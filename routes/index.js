fe9e1e11ecca0f86c07f9c4e310c54e051080cd1
Luis <luisferqk2@gmail.com>
2023-04-21 21:55:03 -0400

    primer commit

---
+++ routes/index.js
@@ -0,0 +1,88 @@
+import express from 'express';
+import { 
+    paginaInicio,
+    paginaNosotros,
+    paginaViajes,
+    paginaTestimoniales,
+    paginainfoviaje } from '../controllers/paginasController.js';
+import {
+    guardarTestimonial } from '../controllers/testimonialesController.js';
+
+const router=express.Router();
+
+router.get('/',paginaInicio);
+router.get('/nosotros',paginaNosotros)
+router.get('/viajes',paginaViajes)
+
+//usando un comodin para acceder a una url diferente dependiendo del parametro dado a la url.
+router.get('/viajes/:slug',paginainfoviaje)
+
+router.get('/testimoniales',paginaTestimoniales)
+router.post('/testimoniales',guardarTestimonial)
+
+export default router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import express from 'express';
+// import { 
+//     paginaInicio, 
+//     paginaNosotros, 
+//     paginaViajes, 
+//     paginaTestimoniales, 
+//     paginaDetalleViaje 
+// } from '../controllers/paginasController.js';
+
+// import { guardarTestimonial } from '../controllers/testimonialesController.js';
+
+// const router = express.Router();
+
+// router.get('/', paginaInicio);
+
+// router.get('/nosotros', paginaNosotros);
+
+// router.get('/viajes', paginaViajes);
+
+// router.get('/viajes/:slug', paginaDetalleViaje);
+
+// router.get('/testimoniales', paginaTestimoniales);
+
+
+// // Cuando Lleno el form
+// router.post('/testimoniales', guardarTestimonial);
+
+
+// export default router;
