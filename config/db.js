d75de418c9ee1517102498466cd3533253f1a4af
Luis <luisferqk2@gmail.com>
2023-04-23 01:04:30 -0400

    primer commit

--- config/db.js
+++ config/db.js
@@ -1,5 +1,6 @@
 import { Sequelize } from "sequelize";
 import dotenv from 'dotenv/config'
+
 console.log(process.env.DB_HOST)
 
 const db = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASS,{
